bd7510f1ed62cf3c4b13a8eb3933edf8866022c8
Rob van der Made <vandermade@kabelfoon.nl>
2026-05-11 21:24:45 +0200

    env

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,15 @@ import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyCG8OaDlFFWSk7hTTc2r-Is-Lrdt_Mubnw',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'productie-app.firebaseapp.com',
+  databaseURL:
+    import.meta.env.VITE_FIREBASE_DATABASE_URL ||
+    'https://productie-app-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'productie-app',
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'productie-app.firebasestorage.app',
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '411427524297',
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:411427524297:web:d4bd076926e62eeb094718',
 }
 
 const hasFirebaseConfig =
